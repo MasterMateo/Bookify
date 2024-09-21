7cf0274723662d8f7ce75654e190488426b355f5
Горлов Матвей Сергеевич <MGorlov@alfabank.ru>
2024-09-21 14:04:16 +0300

    Added RowVersion to Apartment entity, added ConcurrencyException to DbContext

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
-﻿using Bookify.Domain.Abstractions;
+﻿using System.Data;
+using Bookify.Application.Exceptions;
+using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
+
+    private readonly IPublisher _publisher;
+    
     public ApplicationDbContext(DbContextOptions options)
         : base(options)
     {
@@ -15,5 +21,42 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        try
+        {
+            var result = await base.SaveChangesAsync(token);
+
+            await PublishDomainEventAsync();
+
+            return result;
+        }
+        catch (DBConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exeption occured.", ex);
+        }
+    }
+
+    private async Task PublishDomainEventAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
